59c47a618a262e5806561227e52fd64713b17260
YanaAlexa4 <Aleksandrovayana@mail.ru>
2019-12-16 12:02:55 +0700

    Initial commit

---
+++ bootstrap-master/README.md
@@ -0,0 +1,4 @@
+# bootstrap with Sass and gulp
+ https://yana4079.github.io/bootstrap/
+ clone Italian newspaper
+ 
---
+++ bootstrap-master/assets/js/main.js
@@ -0,0 +1,9 @@
+function HideLeftMenu() {
+    document.getElementById('close').style.display = "none";
+    console.log("Привет");
+}
+
+function OpenLeftMenu() {
+    document.getElementById('close').style.display = "block";
+    console.log("Привет");
+}
