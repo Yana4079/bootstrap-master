e713aee1ce1f2f992323c742ef1bb8794932213e
YanaAlexa4 <Aleksandrovayana@mail.ru>
2019-12-20 17:47:28 +0700

    changed left menu

--- bootstrap-master/assets/js/main.js
+++ bootstrap-master/assets/js/main.js
@@ -1,3 +1,19 @@
+// $(document).ready(function () {
+    $(".fa-chevron-down").click(function () { 
+        $(".submenu").click(); 
+    });
+    $(".submenu").click(function (e) { 
+        e.toggle(); 
+    });
+// });
+
+$('.toggle_menu').click(function(){
+    $(this).find('.submenu').toggleClass('hide');
+})
+
+
+
+
 function HideLeftMenu() {
     document.getElementById('close').style.display = "none";
     console.log("Привет");
@@ -7,3 +23,6 @@ function OpenLeftMenu() {
     document.getElementById('close').style.display = "block";
     console.log("Привет");
 }
+
+
+
---
+++ bootstrap-master/assets/js/openSubMenu.js
@@ -0,0 +1,5 @@
+function openSubMenu() {
+    document.getElementsByClassName('submenu__Item').style.opacity = "1";
+    document.getElementsByClassName('submenu').style.height = "20px";
+    console.log("Привет");
+}
